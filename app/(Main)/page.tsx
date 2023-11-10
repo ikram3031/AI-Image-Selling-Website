464adce02e4a0274b944bc24c3072c8a01db85e2
ikram3031 <mdikram.3031@gmail.com>
2023-11-10 17:22:42 +0600

    54:00

---
+++ app/(Main)/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react';
+import Header from '@/components/Header';
+
+type Props = {}
+
+const Page = (props: Props) => {
+  return (
+    <div>
+        <Header activeItem={0}/>
+    </div>
+  )
+}
+
+export default Page
