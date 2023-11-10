40265b151cc3ee98f21c1dedf2b2d26edd53fc84
ikram3031 <mdikram.3031@gmail.com>
2023-11-10 22:18:20 +0600

    09:00

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images:{
-        domains: ['pixner.net']
-    }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,20 @@
-import type { Config } from "tailwindcss";
-const { nextui } = require("@nextui-org/react");
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./utils/styles.ts",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      fontFamily:{
-         Inter:["var(--font-inter)"],
-         Monserrat: ["var(--font-montserrat)"],
-      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [nextui({
-    theme: {}
-  })],
-};
-export default config;
+  plugins: [],
+}
+export default config
