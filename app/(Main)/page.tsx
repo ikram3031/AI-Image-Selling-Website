550c6830f5168929688b77777d15059668771525
ikram3031 <mdikram.3031@gmail.com>
2023-11-10 18:22:59 +0600

    01:25

--- app/(Main)/page.tsx
+++ app/(Main)/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from '@/components/Header';
 import Hero from '@/components/Route/Hero';
 import Image from 'next/image';
+import About from '@/components/Route/About';
 
 type Props = {}
 
@@ -11,6 +12,7 @@ const Page = (props: Props) => {
     <div>
         <Header activeItem={0}/>
         <Hero />
+        
         <Image
           src={"https://pixner.net/aikeu/assets/images/footer/shape-two.png"}
           width={120}
@@ -18,6 +20,11 @@ const Page = (props: Props) => {
           alt=""
           className="absolute right-[-30px]"
         />
+        
+        <br />
+        <div className="w-[95%] md:w-[90%] xl:w-[80%] 2xl:w-[75%] m-auto">
+          <About />
+        </div>
     </div>
   )
 }
